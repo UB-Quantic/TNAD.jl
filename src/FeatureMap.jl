80c7beba1e15153cecfa460c07245f538be290e9
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-04-21 16:21:14 +0200

    Fix partial functions of feature maps

--- src/FeatureMap.jl
+++ src/FeatureMap.jl
@@ -1,7 +1,8 @@
-ϕtrig(; k=1) = x -> ϕtrig(x; k=k)
+ϕtrig(; k) = x -> ϕtrig(x; k=k)
 ϕtrig(x; k=1) = 1 / √k * [f(π / 2^k * x) for f ∈ (cos, sin) for i ∈ 1:k]
 
-ϕfourier(; p=2) = x -> ϕfourier(x; p=p)
+ϕfourier(; p) = x -> ϕfourier(x; p=p)
 ϕfourier(x; p=2) = 1 / p * [abs(sum(cis(2 * π * k * ((p - 1) * x - j) / p) for k ∈ 0:p-1)) for j ∈ 0:p-1]
 
+Φ(; ϕ) = x -> Φ(x; ϕ=ϕ)
 Φ(𝐱::Vector; ϕ=ϕfourier) = map(ϕ, 𝐱)
